4ff2c02ab14602e8761b31089c559f780aeb2d9f
VictorKuznetsov88 <bitv2010@yandex.ru>
2020-07-27 07:32:43 +0300

    Добавлено webdrivermanager в pom.xml

---
+++ src/main/java/ru/geekbrains/java/oop/AuthorizationPage.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.java.oop;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AuthorizationPage {
+
+    @FindBy(css = "[placeholder=\"Email\"]")
+    private WebElement inputLogin;
+
+    @FindBy(css = "[placeholder=\"Пароль\"]")
+    private WebElement inputPassword;
+
+    @FindBy(css = "[id=\"registration-form-button\"]")
+    private WebElement buttonSingIn;
+
+    @FindBy(css = "[id=\"nav\"] [href=\"/courses\"]")
+    private WebElement buttonCourses;
+
+    private WebDriver driver;
+
+    public AuthorizationPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public ContentPage authorization(String login, String password) {
+        inputLogin.sendKeys(login);
+        inputPassword.sendKeys(password);
+
+        buttonSingIn.click();
+
+        return new ContentPage(driver);
+    }
+
+    public WebElement getInputLogin() {
+        return inputLogin;
+    }
+
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
+
+    public WebElement getButtonSingIn() {
+        return buttonSingIn;
+    }
+
+    public WebElement getButton(String nameButton) {
+        if (nameButton == "Курсы"){
+            return buttonCourses;}else {
+            return null;
+        }
+    }
+}
---
+++ src/test/java/ru/geekbrains/java/oop/AuthorizationWebTest.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.java.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.geekbrains.java.oop.BaseWebTest;
+import ru.geekbrains.java.oop.AuthorizationPage;
+
+@DisplayName("Авторизация")
+public class AuthorizationWebTest extends BaseWebTest {
+
+    @DisplayName("Успешная авторизация")
+    @Test
+    public void auth() {
+        driver.get("https://geekbrains.ru/");
+        String login = "hks47018@eoopy.com";
+        String password = "hks47018";
+        String nameButton="Курсы";
+
+//Авторизация проверка главной страницы
+        new AuthorizationPage(driver)
+                .authorization(login,password)
+                .checkNamePage("Главная");
+
+//Нажать в навигации "Курсы"
+        new AuthorizationPage(driver)
+                .getButton(nameButton).click();
+
+//Нажать в шапке сайта курсы
+       driver.findElement(By.xpath("//a[@class='text-dark-dk' and @href='#cour-new']")).click();
+     //  System.out.println(driver.findElement(By.xpath("//a[@class='text-dark-dk' and @href='#cour-new']")).getText());
+
+//Быбрать чекбоксы Бесплатно и Тестирование
+
+       driver.findElement(By.cssSelector("[id=\"filter-0\"]")).click();
+       driver.findElement(By.cssSelector("[id=\"filter-9\"]")).click();
+
+//Проверка выбора
+        Assertions.assertEquals(
+                "Тестирование ПО. Уровень 2\n" +
+                        "Работа с документацией и тестирование приложений",
+                driver.findElement(By.xpath("//a[@class='gb-course-card__wrapper' and @href = '/courses/85']")).getText());
+
+        Assertions.assertEquals(
+                "Тестирование ПО. Уровень 1\n" +
+                        "Теоретические знания и начальный опыт",
+                driver.findElement(By.xpath("//a[@class='gb-course-card__wrapper' and @href = '/courses/84']")).getText());
+
+        //System.out.println(driver.findElement(By.xpath("//a[@class='gb-course-card__wrapper' and @href = '/courses/85']")).getText());
+
+
+    }
+
+}
